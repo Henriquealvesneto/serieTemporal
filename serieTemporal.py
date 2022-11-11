4fd84eec77fb9b0a1d80b7559c0f07c847f2c2d7
Henrique Alves Neto <henriquealves_neto@outlook.com>
2022-11-10 23:37:01 -0400

    first commit

---
+++ serieTemporal.py
@@ -0,0 +1,19 @@
+### Implementar uma solução em Python para estudar o compartamento de uma série temporal com Rgressão Linear ####
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+x = np.array([5, 10, 15, 20, 25, 30]).reshape((-1, 1))
+y = np.array([6, 12, 14, 23, 27, 32])
+
+model = LinearRegression().fit(x, y)
+
+# Premonindo e imprimindo isso
+y_pred = model.predict(x)
+print(f'Dados do teste: ',y, sep='\n')
+print(f'Dados da predição: ',y_pred, sep='\n')
+
+plt.scatter(x, y, c='blue')
+plt.plot(x, y_pred, c='red')
+plt.legend(['Predição', 'Real'])
+plt.show()
